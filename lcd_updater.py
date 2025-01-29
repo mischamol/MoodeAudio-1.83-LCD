770407bcdefb342ba6f289e1b6390e3a48b75dc8
Mischa Mol <mischamol@icloud.com>
2025-01-29 11:18:00 +0100

    Update

--- lcd_updater.py
+++ lcd_updater.py
@@ -1,58 +1,7 @@
 #!/usr/bin/python3
 #
 # Script for displaying MoodeAudio coverart on waveshare 1.83 inch lcd screen
-# don't forget to enable SPI: sudo rasp-config => enable spi  and pip install RPi.GPIO
 # part of this code including some of the included libraries come from the waveshare example files
-# you can place this files under /var/local/www/commandw/
-# enable the lcd updater in moodeaudio under 'configure' -> 'Periphals' or currentsong.txt
-# this file is replaced by a stub after every update, so keep a backup somewhere else
-# also use een systemd watcher on spotmeta.txt to include spotify metadata
-
-# alternatively use een systemd watcher on both currentsong.txt and spotmeta.txt 
-# and place the files in your home folder, as this folder is left untouched during update
-# remember to sudo chmod -R 777 lcd 
-
-#sudo nano /etc/systemd/system/spotwatcher.service
-#[Unit]
-#Description = Run LCD_updater.py on spotmeta change
-#ConditionPathExists=/var/local/www/spotmeta.txt
-#
-#[Service]
-#ExecStart=/usr/bin/python3 ~/lcd/lcd_updater.py
-
-#sudo nano /etc/systemd/system/spotwatcher.path
-#[Unit]
-#Description=Monitor spotmeta.txt and trigger spotwatcher service
-#
-#[Path]
-#PathModified=/var/local/www/spotmeta.txt
-#
-#[Install]
-#WantedBy=multi-user.target
-
-#sudo systemctl daemon-reload
-#sudo systemctl enable --now spotwatcher.path
-
-#sudo nano /etc/systemd/system/currentsong.service
-#[Unit]
-#Description = Run LCD_updater.py on currentsong.txt change
-#ConditionPathExists=/var/local/www/currentsong.txt
-#
-#[Service]
-#ExecStart=/usr/bin/python3 ~/lcd/lcd_updater.py
-
-#sudo nano /etc/systemd/system/currentsong.path
-#[Unit]
-#Description=Monitor currentsong.txt and trigger currentsong service
-#
-#[Path]
-#PathModified=/var/local/www/currentsong.txt
-#
-#[Install]
-#WantedBy=multi-user.target
-
-#sudo systemctl daemon-reload
-#sudo systemctl enable --now currentsong.path
 
 import os
 import sys 
