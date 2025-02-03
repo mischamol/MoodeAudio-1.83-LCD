c7dd86e1a2d6ce74028de4587ed38cc61aa576ff
Mischa Mol <mischamol@Mischas-MacBook.local>
2025-02-03 16:46:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# MoodeAudio Metadata on Waveshare 1.83-inch LCD
+
+Script for displaying MoodeAudio cover art on a Waveshare 1.83-inch LCD screen. This script is loosely based on the Waveshare example and uses the library files and fonts that came with this example. These files are included in the `lib` folder of this repo.<br>
+
+https://www.waveshare.com/wiki/1.83inch_LCD_Module
+
+
+<img src="https://github.com/user-attachments/assets/06b68c89-d994-4f3c-93f7-e2eedc305546" style="width:300px; height:auto;"><br>
+<sub>The LCD in a temporary configuration consisting of an Argon One case and some Danish bricks.</sub><br>
+
+## Prerequisites
+Connect the screen according to the diagram below.
+![image](https://github.com/user-attachments/assets/9180c546-3529-4612-8237-3af47816893e)<br> 
+<sub>Waveshare connection diagram (source: https://www.waveshare.com/wiki/1.83inch_LCD_Module)</sub><br>
+
+Enable SPI: `sudo raspi-config` ➡ Interface Options ➡ enable SPI.
+
+Install the required library: `pip install RPi.GPIO`
+
+## Installation
+You can place `lcd_updater.py` and the `lib` folder under `/var/local/www/commandw/` and enable the LCD updater in MoodeAudio under ➡ Configure ➡ Peripherals. 
+
+Set the permissions: `sudo chmod 777 lcd_updater.py` and `sudo chmod -R 777 bin`
+
+⚠ Note: `lcd_updater.py` is replaced with a stub after every update, so keep a backup elsewhere. Additionally, use a systemd watcher on `spotmeta.txt` to include Spotify metadata.
+
+## Alternative Installation Method
+Instead of enabling the LCD updater in MoodeAudio, you can use a systemd watcher for both `currentsong.txt` and `spotmeta.txt`. As a result, you can place this repository wherever you want—for example, in your home folder (~), which remains untouched during updates. Personally, I prefer this method, as a got tired of copying everything back after every update.
+
+Ensure the correct permissions: `sudo chmod -R 777 lcd`
+
+## Example of systemd watcher configuration for `spotmeta.txt`
+`sudo nano /etc/systemd/system/spotwatcher.service`
+```
+[Unit]
+Description = Run LCD_updater.py on spotmeta change
+ConditionPathExists=/var/local/www/spotmeta.txt
+
+[Service]
+ExecStart=/usr/bin/python3 ~/lcd/lcd_updater.py
+```
+`sudo nano /etc/systemd/system/spotwatcher.path`
+```
+[Unit]
+Description=Monitor spotmeta.txt and trigger spotwatcher service
+
+[Path]
+PathModified=/var/local/www/spotmeta.txt
+
+[Install]
+WantedBy=multi-user.target
+```
+```
+sudo systemctl daemon-reload
+sudo systemctl enable --now spotwatcher.path
+```
+
+## Example of optional systemd watcher configuration for `currentsong.txt`
+`sudo nano /etc/systemd/system/currentsong.service`
+```
+[Unit]
+Description = Run LCD_updater.py on currentsong.txt change
+ConditionPathExists=/var/local/www/currentsong.txt
+
+[Service]
+ExecStart=/usr/bin/python3 ~/lcd/lcd_updater.py
+```
+`sudo nano /etc/systemd/system/currentsong.path`
+```
+[Unit]
+Description=Monitor currentsong.txt and trigger currentsong service
+
+[Path]
+PathModified=/var/local/www/currentsong.txt
+
+[Install]
+WantedBy=multi-user.target
+```
+```
+sudo systemctl daemon-reload
+sudo systemctl enable --now currentsong.path
+```
+
+Enjoy!
---
+++ lcd_updater.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+#
+# Script for displaying MoodeAudio coverart on Waveshare 1.83 inch lcd screen
+# This script is loosely based on the Waveshare example and uses the library files and fonts 
+# that came with this example. These files are included in the lib folder of this repo.
+
+import os
+import sys 
+import logging
+import spidev as SPI
+from lib import LCD_1inch83 #Waveshare lib
+from PIL import Image, ImageOps, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0 
+device = 0 
+logging.basicConfig(level = logging.WARNING)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # set working dir
+Font = ImageFont.truetype("lib/Font02.ttf", 18)
+
+def roundImage(image, radius) -> Image.Image:
+    image = image.convert("RGBA")
+    mask = Image.new("L", image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    width, height = image.size
+    draw.rounded_rectangle((0, 0, width, height), radius=radius, fill=255)
+    rounded_image = Image.new("RGBA", image.size)
+    rounded_image.paste(image, (0, 0), mask=mask)
+    return rounded_image
+
+
+def getMetaData() -> tuple[str, str, str]:
+    data = {}
+    with open('/var/local/www/currentsong.txt', 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=', 1)  # split on first '='
+            data[key] = value
+        if data.get("file")=="Spotify Active": #spotify uses spotmeta.txt instead of currentsong.txt
+            with open('/var/local/www/spotmeta.txt', 'r') as spotfile:
+                spotdata=spotfile.readline().split('~~~')
+            song=spotdata[0]
+            artist=spotdata[1]
+            imageurl = next((item for item in spotdata if item.startswith("https://")), None).rstrip() 
+            #return elemment that starts with https:// instead of using index (url index changes sometimes)
+        else: #local file or radio stream
+            coverurl=data.get("coverurl").replace('%2F', '/')
+            if coverurl.startswith('/'):
+                coverurl = coverurl[1:] #strip first "/" which is added when playing local files, but not with streams)
+            imageurl='http://localhost/'+ coverurl
+            song=data.get("title")
+            artist=data.get("artist")
+        #print(imageurl)
+    return imageurl, song, artist
+
+try:
+    disp = LCD_1inch83.LCD_1inch83(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp.Init()
+    disp.clear()
+    disp.bl_DutyCycle(50) #set backlight brightness 
+    imageurl, song, artist =getMetaData()
+    response = requests.get(imageurl)
+    if response.status_code!=200: #if not ok
+        response = requests.get("http://localhost/images/default-album-cover.png") #fallback to default cover         
+    image=Image.open(BytesIO(response.content))
+    image = image.resize((240, 240))
+    image=roundImage(image, 40)
+    image = ImageOps.pad(image, (240, 280), method=Image.Resampling.BILINEAR, color=(0, 0, 0), centering=(0.5, 0)) #padding with black 
+    draw = ImageDraw.Draw(image)
+    draw.text((25, 240), artist , fill = "WHITE",font=Font)
+    draw.text((25, 260), song , fill = "WHITE",font=Font)
+    disp.ShowImage(image)
+    disp.module_exit()
+except IOError as e:
+    logging.info(e)  
