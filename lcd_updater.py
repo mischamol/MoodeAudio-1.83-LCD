6bf2e271f50d9da34332be308bbdb35edf02d14c
Mischa Mol <mischamol@icloud.com>
2025-03-11 09:55:13 +0100

    code cleaning

--- lcd_updater.py
+++ lcd_updater.py
@@ -23,17 +23,6 @@ logging.basicConfig(level = logging.WARNING)
 os.chdir(os.path.dirname(os.path.abspath(__file__)))  # set working dir
 Font = ImageFont.truetype("lib/Font02.ttf", 18)
 
-def roundImage(image, radius) -> Image.Image:
-    image = image.convert("RGBA")
-    mask = Image.new("L", image.size, 0)
-    draw = ImageDraw.Draw(mask)
-    width, height = image.size
-    draw.rounded_rectangle((0, 0, width, height), radius=radius, fill=255)
-    rounded_image = Image.new("RGBA", image.size)
-    rounded_image.paste(image, (0, 0), mask=mask)
-    return rounded_image
-
-
 def getMetaData() -> tuple[str, str, str]:
     data = {}
     with open('/var/local/www/currentsong.txt', 'r') as file:
@@ -59,22 +48,40 @@ def getMetaData() -> tuple[str, str, str]:
         #print(imageurl)
     return imageurl, song, artist
 
-try:
-    disp = LCD_1inch83.LCD_1inch83(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-    disp.Init()
-    disp.clear()
-    disp.bl_DutyCycle(50) #set backlight brightness 
-    imageurl, song, artist =getMetaData()
+def getImage(imageurl) -> Image.Image:
     response = requests.get(imageurl)
     if response.status_code!=200: #if not ok
         response = requests.get("http://localhost/images/default-album-cover.png") #fallback to default cover         
     image=Image.open(BytesIO(response.content))
+    return image
+
+def roundImage(image, radius) -> Image.Image:
+    image = image.convert("RGBA")
+    mask = Image.new("L", image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    width, height = image.size
+    draw.rounded_rectangle((0, 0, width, height), radius=radius, fill=255)
+    rounded_image = Image.new("RGBA", image.size)
+    rounded_image.paste(image, (0, 0), mask=mask)
+    return rounded_image
+
+def drawImage(image, song, artist)-> Image.Image:
     image = image.resize((240, 240))
     image=roundImage(image, 40)
     image = ImageOps.pad(image, (240, 280), method=Image.Resampling.BILINEAR, color=(0, 0, 0), centering=(0.5, 0)) #padding with black 
     draw = ImageDraw.Draw(image)
     draw.text((25, 240), artist , fill = "WHITE",font=Font)
     draw.text((25, 260), song , fill = "WHITE",font=Font)
+    return image
+
+try:
+    disp = LCD_1inch83.LCD_1inch83(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp.Init()
+    disp.clear()
+    disp.bl_DutyCycle(50) #set backlight brightness 
+    imageurl, song, artist =getMetaData()
+    coverart=getImage(imageurl)
+    image=drawImage(coverart, song, artist)
     disp.ShowImage(image)
     disp.module_exit()
 except IOError as e:
